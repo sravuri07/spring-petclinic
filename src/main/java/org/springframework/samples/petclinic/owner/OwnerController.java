5225b010dcaa9a36357abebaa61747499490c27c
Sampath Ravuri <ravurisampath07@gmail.com>
2025-10-19 13:57:26 -0400

    Small update in the comment

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -172,7 +172,8 @@ class OwnerController {
 		mav.addObject(owner);
 		return mav;
 	}
-// Delete Owner Button
+
+	// Delete Owner Button
 	@PostMapping("/owners/{ownerId}/delete")
 	public String deleteOwner(Owner owner, RedirectAttributes redirectAttributes) {
 		try {
